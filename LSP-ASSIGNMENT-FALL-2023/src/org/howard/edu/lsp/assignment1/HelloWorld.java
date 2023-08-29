ccd1ddf6f05df586888029b29649ea5b88e194ea
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-08-28 22:50:32 -0400

    Initial commit

---
+++ LSP-ASSIGNMENT-FALL-2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	/**
+	 * 
+	 * SIYAM SHAHRIAR
+	 * @03043178
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
