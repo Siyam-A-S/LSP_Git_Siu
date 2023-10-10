d46dcfa11c93eb0ff0af403d106e443278d2c345
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-10-09 23:06:14 -0400

    2nd commit

--- LSP-ASSIGNMENT-FALL-2023/src/org/howard/edu/lsp/assignment4/IntegerSet.java
+++ LSP-ASSIGNMENT-FALL-2023/src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -1,10 +1,32 @@
 package org.howard.edu.lsp.assignment4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntegerSet {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private List<Integer> set = new ArrayList<Integer>();
+	
+	/**
+	 * This method add an integer to the Integer Set
+	 * @param item
+	 */
+	public void add(int item) {
+		if (!set.contains(item) ) {
+			set.add(item);
+		}
+	}
+	/**
+	 * A void class which clears out the Integer set
+	 */
+	public void clear() {
+		set.clear();
+	}
+	/**
+	 * This method return the string representation of the Integer set
+	 * @return String
+	 */
+	public String toString() {
+		return set.toString();
 	}
 
 }
