f96466471167afc167b17b8ca9bb14c8f8067b4e
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-10-14 09:31:09 -0400

    Added new methods such as equals, length, contains, remove

--- LSP-ASSIGNMENT-FALL-2023/src/org/howard/edu/lsp/assignment4/IntegerSet.java
+++ LSP-ASSIGNMENT-FALL-2023/src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -7,7 +7,7 @@ public class IntegerSet {
 	private List<Integer> set = new ArrayList<Integer>();
 	
 	/**
-	 * This method add an integer to the Integer Set
+	 * This method add an integer to the Integer Set if it is already not in it
 	 * @param item
 	 */
 	public void add(int item) {
@@ -28,5 +28,41 @@ public class IntegerSet {
 	public String toString() {
 		return set.toString();
 	}
+	/**
+	 * This method removes an item from the set if the item exists
+	 * @param item
+	 */
+	public void remove(int item) {
+		if (set.contains(item) ) {
+			set.remove(Integer.valueOf(item));
+		}
+		
+	}
+	/**
+	 * This method returns true if the two sets are equal
+	 */
+	public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        IntegerSet that = (IntegerSet) o;
+
+        return set.containsAll(that.set) && that.set.containsAll(set);
+    }
+	/**
+	 * This method returns the number of elements in the set
+	 * @return length of the set
+	 */
+	public int length() {
+        return set.size();
+    }
+	/**
+	 * This method returns true if the set contains the value
+	 * @param value
+	 * @return boolean
+	 */
+	public boolean contains(int value) {
+        return set.contains(value);
+    }
 
 }
